6cb15b25f3bceaba82a209a9772efffc72aed7f1
Barak Schiller <barak@we7.com>
2014-11-13 17:32:45 +0000

    Initial state

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='cronen',
+      version='1.0',
+      description='Micro cron library',
+      author='Barak Schiller',
+      author_email='bschiller@gmail.com',
+      packages=['cronen'],
+     )
