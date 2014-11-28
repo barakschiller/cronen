e8feed315f7f7e7d043cb9c3a60b94ffcbdbcdc9
Barak Schiller <baraks@blinkbox.com>
2014-11-28 13:09:46 +0000

    Fixed package structure

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import setuptools
 
 setup(name='cronen',
       version='1.0',
+      package_dir={'': 'src'},
       description='Micro cron library',
       author='Barak Schiller',
       author_email='bschiller@gmail.com',
-      packages=['cronen'],
+      packages=setuptools.find_packages(where='src'),
      )
