fbfd91d878b9f6827b1097556cabe37357e89ab6
Barak Schiller <barak@blinkbox.com>
2014-11-29 21:11:38 +0000

    Added option for error handler

--- src/cronen/__init__.py
+++ src/cronen/__init__.py
@@ -21,11 +21,12 @@ class ScheduledJob(object):
 	A scheduled job with a web monitoring interface.
 	:param func The job function (no parameters allowed, you can use functools to bind parameters).
 	"""
-	def __init__(self, name, func, trigger):
+	def __init__(self, name, func, trigger, error_handler):
 		self.name = name
 		self.func = func
 		self.trigger = trigger
 		self.last_run = RunResult('Never', 'Never', None)
+		self.error_handler = error_handler
 
 	def run(self):
 		try:
@@ -39,12 +40,13 @@ class ScheduledJob(object):
 				None)
 		except Exception as e:
 			log.exception('Error running job')
-			#TODO: add error email support?
+			
 			end_time = datetime.now()
 			self.last_run = RunResult(
 				self.format_timestamp(start_time), 
 				self.format_timestamp(end_time), 
 				str(e))
+			self.error_handler(self, e)
 		finally:
 			print 'Done running {}'.format(self.name)
 
@@ -54,12 +56,24 @@ class ScheduledJob(object):
 	
 
 class Cronen(object):
-	def __init__(self, port):
+
+	@staticmethod
+	def null_error_handler(job, exception):
+		pass
+
+	def __init__(self, port, error_handler=null_error_handler):
+		"""
+		:param port port to be used for web server
+		:param error_handler error handler to be called in case of job failure
+			function(ScheduledJob, Exception)
+		"""
 		self.port = port
 		self.jobs = {}
+		self.error_handler = error_handler
+
 
 	def add_job(self, name, func, trigger):
-		self.jobs[name] = ScheduledJob(name, func, trigger)
+		self.jobs[name] = ScheduledJob(name, func, trigger, self.error_handler)
 
 
 	def start(self):
@@ -70,7 +84,6 @@ class Cronen(object):
 		self._start_web_server()
 
 	def _start_scheduler(self):
-		# TODO: add scheduling configuration
 		for job in self.jobs.values():
 			job.trigger.do(job.run)
 		
@@ -87,7 +100,7 @@ class Cronen(object):
 			time.sleep(1)
 
 	def _start_web_server(self):
-		# setup routes
+
 		@bottle.route('/run/<name>')
 		def web_run(name):
 			logging.info('Running job {} manually'.format(name))
