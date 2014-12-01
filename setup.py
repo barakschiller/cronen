7e64ba81291c66c9f9677ec7ad0a3956caa56634
Barak Schiller <baraks@blinkbox.com>
2014-12-01 14:00:23 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup, find_packages
 
 setup(name='cronen',
       version='1.0',
-      package_dir={'': 'src'},
       description='Micro cron library',
       author='Barak Schiller',
       author_email='bschiller@gmail.com',
-      packages=find_packages(where='src'),
+      packages=find_packages(),
       install_requires=[
             'bottle',
             ]
