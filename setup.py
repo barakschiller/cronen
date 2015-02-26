8ff976afefcd3e9da0ef7cb6880882f45e7d32c3
Barak Schiller <barak@we7.com>
2015-02-26 13:58:05 +0000

    Improved setup.py for release to pypi

--- setup.py
+++ setup.py
@@ -2,13 +2,15 @@
 from setuptools import setup, find_packages
 
 setup(name='cronen',
+      packages=find_packages(),
+      install_requires=[
+      	'bottle',
+  		],
       version='1.0',
-      description='Micro cron library',
+      description='A mini cron library for python. Allows scheduling jobs on a single node and provides a simple web interface for monitoring and manual triggering.',
       author='Barak Schiller',
       author_email='bschiller@gmail.com',
       url='https://github.com/barakschiller/cronen',
-      packages=find_packages(),
-      install_requires=[
-            'bottle',
-            ]
+      download_url='https://github.com/barakschiller/cronen/tarball/1.0',
+      keywords=['cron'],
      )
