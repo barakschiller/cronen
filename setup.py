e83d69eec23c9a6ba7b8fa117ef24498e8782b77
Barak Schiller <barak@we7.com>
2015-02-26 13:58:44 +0000

    Spacing

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ from setuptools import setup, find_packages
 
 setup(name='cronen',
       packages=find_packages(),
-      install_requires=[
-      	'bottle',
-  		],
+      install_requires=['bottle',],
       version='1.0',
       description='A mini cron library for python. Allows scheduling jobs on a single node and provides a simple web interface for monitoring and manual triggering.',
       author='Barak Schiller',
@@ -13,4 +11,4 @@ setup(name='cronen',
       url='https://github.com/barakschiller/cronen',
       download_url='https://github.com/barakschiller/cronen/tarball/1.0',
       keywords=['cron'],
-     )
+      )
