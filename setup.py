91c25884d0cab165caebe86ddb88d1bdb619a9eb
Barak Schiller <barak@we7.com>
2015-02-26 14:28:37 +0000

    Added information on whether the job is currently running to the job state.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(name='cronen',
       packages=find_packages(),
       install_requires=['bottle',],
-      version='1.0',
+      version='1.1',
       description='A mini cron library for python. Allows scheduling jobs on a single node and provides a simple web interface for monitoring and manual triggering.',
       author='Barak Schiller',
       author_email='bschiller@gmail.com',
